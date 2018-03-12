ba8d460c96f9f21487e1056a17e1eb604ece96b6
“kenkun2018 <532123036@qq.com>
2018-03-12 10:45:53 +0800

    update

--- selenium_chrome.py
+++ selenium_chrome.py
@@ -22,7 +22,7 @@ loginbutton = br.find_element_by_xpath('//*[@id="frmLogin"]/div/div[3]/div/div[5
 loginbutton.click()
 
 
-with open(r'E:\pyfile\tmpfile\parkNo.txt') as f:
+with open(r'E:\myGithub\pyproject\parknum.txt') as f:
 	data = f.readlines()
 
 rows = []
@@ -30,40 +30,54 @@ for l in data:
 	rows.append(l.split())
 
 
+#br.get('http://www.jslife.com.cn/jsstApp/crm/businesserAction.action')
+#
+#shopnum = br.find_element_by_xpath('//*[@id="businesserCode"]')
+#shopnum.send_keys('880075201007846')
+#submit = br.find_element_by_xpath('//*[@id="searchBtn"]/a')
+#submit.click()
+#time.sleep(0.5)
+#modifybtn = br.find_element_by_xpath('//*[@src="http://www.jslife.com.cn:80/jsstApp/images/edit.gif"]')
+#modifybtn.click()
+#
+#daikouframe = br.find_element_by_xpath('/html/body/div[3]/div/iframe')
+#br.switch_to.frame(daikouframe)
+#
+#daikou = br.find_element_by_xpath('//*[@id="isSignatoryChecked"]')
+#
+#br.switch_to.default_content()
+#closeframe = br.find_element_by_xpath('/html/body/div[3]/table/tbody/tr/td[3]/div')
+#closeframe.click()
+
+
+
+
+br.get('http://www.jslife.com.cn/jsstApp/crm/parkCodeManagerAction.action')
+
+print (dir(br))
+
+rows2 = []
+for l in rows:
+	parkinput = br.find_element_by_xpath('//*[@id="code"]')
+	parkinput.clear()
+	parkinput.send_keys(l[0])
+	searchbut = br.find_element_by_xpath('//*[@id="searchBtn"]/a')
+	searchbut.click()
+	time.sleep(0.5)
+	shopNo = br.find_element_by_xpath('//*[@aria-describedby="gridTable_busCode"]')
+	print (shopNo.text)
+	l.append(shopNo.text)
+	rows2.append(l)
+
+pprint (rows2)
+
 br.get('http://www.jslife.com.cn/jsstApp/crm/businesserAction.action')
 
-shopnum = br.find_element_by_xpath('//*[@id="businesserCode"]')
-shopnum.send_keys('880075201007846')
-submit = br.find_element_by_xpath('//*[@id="searchBtn"]/a')
-submit.click()
-time.sleep(0.5)
-modifybtn = br.find_element_by_xpath('//*[@src="http://www.jslife.com.cn:80/jsstApp/images/edit.gif"]')
-modifybtn.click()
-
-daikouframe = br.find_element_by_xpath('/html/body/div[3]/div/iframe')
-br.switch_to.frame(daikouframe)
-
-daikou = br.find_element_by_xpath('//*[@id="isSignatoryChecked"]')
-
-br.switch_to.default_content()
-closeframe = br.find_element_by_xpath('/html/body/div[3]/table/tbody/tr/td[3]/div')
-closeframe.click()
-
-#br.get('http://www.jslife.com.cn/jsstApp/crm/parkCodeManagerAction.action')
-#
-#print (dir(br))
-#
-#rows2 = []
-#for l in rows:
-#	parkinput = br.find_element_by_xpath('//*[@id="code"]')
-#	parkinput.clear()
-#	parkinput.send_keys(l[2])
-#	searchbut = br.find_element_by_xpath('//*[@id="searchBtn"]/a')
-#	searchbut.click()
-#	time.sleep(0.5)
-#	shopNo = br.find_element_by_xpath('//*[@aria-describedby="gridTable_busCode"]')
-#	print (shopNo.text)
-#	l.append(shopNo.text)
-#	rows2.append(l)
-#
-#pprint (rows2)
+for l in rows2:
+	shopnum = br.find_element_by_xpath('//*[@id="businesserCode"]')
+	shopnum.clear()
+	shopnum.send_keys(l[2])
+	submit = br.find_element_by_xpath('//*[@id="searchBtn"]/a')
+	submit.click()
+	print (f"{l[0]} {l[1]} {l[2]}")
+	os.system('pause')
